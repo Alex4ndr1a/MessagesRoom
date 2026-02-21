99b1eaf1d1ba1189dda65de07b34beffd040edf2
Matias A. <lex0ss2062@gmail.com>
2026-02-21 14:43:05 -0300

    refactor: Refactoring with prettier for code convention on the backend code.

--- src/db_handler.ts
+++ src/db_handler.ts
@@ -1,53 +1,53 @@
-import pgPromise from "pg-promise"
+import pgPromise from "pg-promise";
 const pgp = pgPromise();
 import { genSalt, hash } from "bcrypt";
 
 const db = pgp({
-	host: "localhost",
-	port: 5432,
-	database: "MessageRoom", // pending
-	password: undefined,
-	max: 30
+    host: "localhost",
+    port: 5432,
+    database: "MessageRoom", // pending
+    password: undefined,
+    max: 30,
 });
 
-(async() => {
-	try {
-		await db.one("SELECT 1");
-	} catch (error) {
-		console.log("Connection failed, check if the database is up");
-		console.error(error);
-		process.exit(1);
-	}
+(async () => {
+    try {
+        await db.one("SELECT 1");
+    } catch (error) {
+        console.log(
+            "Connection failed, check if the database is up or if permissions are set",
+        );
+        console.error(error);
+        process.exit(1);
+    }
 })();
 
 export async function initDataBase() {
-	let query1 = `SELECT EXISTS (
+    let getTableQuery = `SELECT EXISTS (
 		SELECT FROM information_schema.tables 
 		WHERE  table_schema = 'public'
 		AND    table_name   = $1
-	);`
+	);`;
 
-	let credentials = await db.one(query1, ['credentials']);
-	let users = await db.one(query1, ['users']);
+    let credentials = await db.one(getTableQuery, ["credentials"]);
+    let users = await db.one(getTableQuery, ["users"]);
 
-	if (!users.exists) {
-		let usersTableQuery = 
-			`CREATE TABLE users (
+    if (!users.exists) {
+        let usersTableQuery = `CREATE TABLE users (
 				id SERIAL PRIMARY KEY,
 				user_name VARCHAR(30),
 				user_creation_date TIMESTAMP,
 				age SMALLINT
 		);`;
 
-		try {
-			await db.none(usersTableQuery);
-		} catch (e) {
-			console.error(e);
-		}
-	}
-	if (!credentials.exists) {
-		let credentialsTableQuery = 
-			`CREATE TABLE credentials (
+        try {
+            await db.none(usersTableQuery);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    if (!credentials.exists) {
+        let credentialsTableQuery = `CREATE TABLE credentials (
 				id SERIAL PRIMARY KEY,
 				email VARCHAR(255),
 				password CHAR(60),
@@ -55,52 +55,73 @@ export async function initDataBase() {
 				user_id INTEGER REFERENCES users(id)
 		);`;
 
-		try {
-			await db.none(credentialsTableQuery);
-		} catch (e) {
-			console.error(e);
-		}
-	}
-};
-
-export async function introduceCredentials(userName: string, email: string, password: string): Promise<string> {
-	try {
-		let salt = await genSalt(10)
-		let hashedPass = await hash(password, salt)
-
-		let check_user = await db.any("SELECT 1 FROM users WHERE user_name = $1", [userName]) 
-
-		if (check_user.length > 0) {
-			throw new Error("CREDENTIAL_CONFLICT");
-		}
-
-		let userID = await db.one(`INSERT INTO users (user_name) VALUES ($1) RETURNING id`, [userName]);
-
-		await db.none(`INSERT INTO credentials (email, password, salt, user_id)
-					  VALUES ($1, $2, $3, $4)`, [email, hashedPass, salt, userID.id]);
-
-		return userName;
-	} catch (error) {
-		if (error instanceof Error && error.message == "CREDENTIAL_CONFLICT") 
-			throw error;
-
-		throw new Error("INTERNAL_SERVER_ERROR");
-	}
+        try {
+            await db.none(credentialsTableQuery);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
 
-export async function loginUser(introducedEmail: string, introducedPassword: string): Promise<string>{
-	let data = await db.oneOrNone("SELECT salt, password, user_id FROM credentials WHERE email = $1", [introducedEmail]);
+export async function introduceCredentials(
+    userName: string,
+    email: string,
+    password: string,
+): Promise<string> {
+    try {
+        let salt = await genSalt(10);
+        let hashedPass = await hash(password, salt);
 
-	if (!data) {
-		throw new Error("EMAIL_NOT_FOUND");
-	}
+        let check_user = await db.any(
+            "SELECT 1 FROM users WHERE user_name = $1",
+            [userName],
+        );
 
-	let hashedPass = await hash(introducedPassword, data.salt);
+        if (check_user.length > 0) {
+            throw new Error("CREDENTIAL_CONFLICT");
+        }
 
-	if (hashedPass !== data.password) {
-		throw new Error("INCORRECT_PASSWORD")
-	}
+        let userID = await db.one(
+            `INSERT INTO users (user_name) VALUES ($1) RETURNING id`,
+            [userName],
+        );
 
-	let userName = await db.one("SELECT user_name FROM users WHERE id = $1", [data.user_id]);
-	return userName.user_name;
+        await db.none(
+            `INSERT INTO credentials (email, password, salt, user_id)
+					  VALUES ($1, $2, $3, $4)`,
+            [email, hashedPass, salt, userID.id],
+        );
+
+        return userName;
+    } catch (error) {
+        if (error instanceof Error && error.message == "CREDENTIAL_CONFLICT")
+            throw error;
+
+        throw new Error("INTERNAL_SERVER_ERROR");
+    }
+}
+
+export async function loginUser(
+    introducedEmail: string,
+    introducedPassword: string,
+): Promise<string> {
+    let data = await db.oneOrNone(
+        "SELECT salt, password, user_id FROM credentials WHERE email = $1",
+        [introducedEmail],
+    );
+
+    if (!data) {
+        throw new Error("EMAIL_NOT_FOUND");
+    }
+
+    let hashedPass = await hash(introducedPassword, data.salt);
+
+    if (hashedPass !== data.password) {
+        throw new Error("INCORRECT_PASSWORD");
+    }
+
+    let userName = await db.one("SELECT user_name FROM users WHERE id = $1", [
+        data.user_id,
+    ]);
+    return userName.user_name;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response, NextFunction } from "express"
+import express, { Request, Response, NextFunction } from "express";
 import { existsSync, readFileSync, statSync } from "fs";
-import * as https from "https";
+import { createServer } from "https";
 import { WebSocketServer } from "ws";
 import cookieParser from "cookie-parser";
 import { initDataBase, introduceCredentials, loginUser } from "./db_handler";
@@ -11,38 +11,39 @@ import path from "path";
 const app = express();
 const redisClient = createClient();
 
-function findBaseDirectory(processDir = process.cwd()): string | null {
-  let currentDir = statSync(processDir).isDirectory()
-    ? processDir
-    : path.dirname(processDir);
+function findBaseDirectory(): string | null {
+    const processDir = process.cwd();
+    let currentDir = statSync(processDir).isDirectory()
+        ? processDir
+        : path.dirname(processDir);
 
-  while (true) {
-    const candidate = path.join(currentDir, "package.json");
+    while (true) {
+        const candidate = path.join(currentDir, "package.json");
 
-    if (existsSync(candidate)) {
-      return path.dirname(candidate);
+        if (existsSync(candidate)) {
+            return path.dirname(candidate);
+        }
+
+        const parentDir = path.dirname(currentDir);
+        if (parentDir === currentDir) break;
+
+        currentDir = parentDir;
     }
 
-    const parentDir = path.dirname(currentDir);
-    if (parentDir === currentDir) break;
-
-    currentDir = parentDir;
-  }
-
-  return null;
+    return null;
 }
 
 const BUILD_DIR = findBaseDirectory() + "/build/";
 const PUBLIC_DIR = findBaseDirectory() + "/public/";
-const REACT_DIR = PUBLIC_DIR + "/app/";
+const REACT_DIR = PUBLIC_DIR + "app/";
 
-const SESSION_DURATION = 60 * 60 * 24 * 7 * 1000 // Seven days
+const SESSION_DURATION = 60 * 60 * 24 * 7 * 1000; // Seven days
 
 const sessionDates = () => {
-	const now = new Date();
-	now.setUTCDate(now.getUTCDate() + 7);
-	return now;
-}
+    const now = new Date();
+    now.setUTCDate(now.getUTCDate() + 7);
+    return now;
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -51,176 +52,194 @@ app.use(express.static(PUBLIC_DIR));
 const VALIDURLS = "^(/signin|/login|/)$";
 
 function isValidUrl(req: Request, res: Response, next: NextFunction): void {
-	// This isn't gonna be good for when I create chatrooms with the resource
-	// "/chatroom/{chatroom_id}, because the matches will become complicated,
-	// unless I decide to first verify that the url starts first with
-	// "/chatroom" resource
-	if (req.method === "GET" && !req.url.match(VALIDURLS)) {
-		res.status(401).send("Resource doesn't exists");
-		return;
-	}
+    // This isn't gonna be good for when I create chatrooms with the resource
+    // "/chatroom/{chatroom_id}, because the matches will become complicated,
+    // unless I decide to first verify that the url starts first with
+    // "/chatroom" resource
+    if (req.method === "GET" && !req.url.match(VALIDURLS)) {
+        res.status(401).send("Resource doesn't exists");
+        return;
+    }
 
-	return next();
+    return next();
 }
 
-function authorizeUser(req: Request, res: Response, next: NextFunction): void {
-	if (Object.keys(req.cookies).length > 0) { // Object not empty
-		// TODO: Make it so the session ids last a certain amount of time,
-		// and then make it so with every request to the website, the session
-		// id is refreshed
-		redisClient.get(req.cookies.id)
-		.then((sessionExists) => {
-			if (!sessionExists) {
-				res.status(401).redirect("/login");
-			} else {
-				return next();
-			}
-		})
-		.catch(err => {
-			console.log(err);
-			res.status(505).send("Internal server error");
-		})
-	}
-	else
-		return res.status(401).redirect("/login");
+async function authorizeUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+): Promise<void> {
+    if (Object.keys(req.cookies).length > 0) {
+        let sessionExists: string | null = null;
+        try {
+            sessionExists = await redisClient.get(req.cookies.id);
+        } catch (err) {
+            res.status(505).send("Internal server error");
+            return;
+        }
+
+        if (!sessionExists) {
+            res.status(401).redirect("/login");
+        } else {
+            return next();
+        }
+    } else {
+        return res.status(401).redirect("/login");
+    }
 }
-
-
-app.get("/", authorizeUser, async function(req, res) {
-	let sessionId = await redisClient.keys(req.cookies.id);
-
-	let newId = randomBytes(32).toString("hex");
-	await redisClient.set(newId, 1, { EX: SESSION_DURATION / 1000 });
-
-	if (sessionId) {
-		await redisClient.del(sessionId[0]);
-	}
-
-	res
-	.cookie("id", `${newId}`, {httpOnly: true, secure: true, expires: sessionDates()})
-	.sendFile(REACT_DIR + "index.html");
-})
 
 app.use(express.static(REACT_DIR));
 
+app.get("/", authorizeUser, async function (req, res) {
+    let sessionId = await redisClient.keys(req.cookies.id);
+
+    let newId = randomBytes(32).toString("hex");
+    await redisClient.set(newId, 1, { EX: SESSION_DURATION / 1000 });
+
+    if (sessionId) {
+        await redisClient.del(sessionId[0]);
+    }
+
+    res.cookie("id", `${newId}`, {
+        httpOnly: true,
+        secure: true,
+        expires: sessionDates(),
+    }).sendFile(REACT_DIR + "index.html");
+});
+
 app.route("/login")
-.get((req, res) => {
-	res.sendFile(PUBLIC_DIR + "login/login.html");
-})
-.post(async function(req, res) {
-	try {
-		let userName = await loginUser(req.body.email, req.body.password);
-		let sessionId = randomBytes(32).toString("hex");
+    .get((_, res) => {
+        res.sendFile(PUBLIC_DIR + "login/login.html");
+    })
+    .post(async function (req, res) {
+        try {
+            let userName = await loginUser(req.body.email, req.body.password);
+            let sessionId = randomBytes(32).toString("hex");
 
-		await redisClient.set(sessionId, 1);
+            await redisClient.set(sessionId, 1);
 
-		res.status(200)
-		.cookie("id", `${sessionId}`, {httpOnly: true, secure: true, expires: sessionDates()})
-		.cookie("username", `${userName}`, {secure: true, expires: sessionDates()}) 
-		.redirect("/")
-		// I don't know if this is a good way to do it, but its the only way
-		// I can think of
-	} catch (error) {
-		if (error instanceof Error) {
-			if (error.message === "EMAIL_NOT_FOUND")
-				res.status(401).send("There's no account with this email yet\n");
-			else if (error.message === "INCORRECT_PASSWORD")
-				res.status(401).send("Introduced password does not match with the email\n");
-			else
-				res.status(505).send("Internal server error");
-		}
-	}
-})
+            res.status(200)
+                .cookie("id", `${sessionId}`, {
+                    httpOnly: true,
+                    secure: true,
+                    expires: sessionDates(),
+                })
+                .cookie("username", `${userName}`, {
+                    secure: true,
+                    expires: sessionDates(),
+                })
+                .redirect("/");
+            // I don't know if this is a good way to do it, but its the only way
+            // I can think of
+        } catch (error) {
+            if (error instanceof Error) {
+                if (error.message === "EMAIL_NOT_FOUND")
+                    res.status(401).send(
+                        "There's no account with this email yet\n",
+                    );
+                else if (error.message === "INCORRECT_PASSWORD")
+                    res.status(401).send(
+                        "Introduced password does not match with the email\n",
+                    );
+                else res.status(505).send("Internal server error");
+            }
+        }
+    });
 
 app.route("/signin")
-.get((req, res) => {
-	res.sendFile(PUBLIC_DIR + "signin/signin.html");
-})
-.post(async function(req, res) {
-	try {
-		let userName = await introduceCredentials(req.body.fullname, req.body.email, req.body.password);
-		let sessionId = randomBytes(32).toString("hex");
+    .get((req, res) => {
+        res.sendFile(PUBLIC_DIR + "signin/signin.html");
+    })
+    .post(async function (req, res) {
+        try {
+            let userName = await introduceCredentials(
+                req.body.fullname,
+                req.body.email,
+                req.body.password,
+            );
+            let sessionId = randomBytes(32).toString("hex");
 
-		await redisClient.set(sessionId, 1);
-		res.status(200)
-		.cookie("id", `${sessionId}`, {httpOnly: true, secure: true, expires: sessionDates()})
-		.cookie("username", `${userName}`, {secure: true, expires: sessionDates()}) 
-		.redirect("/")
-		
-	} catch (error) {
-		if (error instanceof Error) {
-			if (error.message === "CREDENTIAL_CONFLICT")
-				res.status(409).send("Sorry, the username you introduced is already in use\n");
-			else
-				res.status(505).send("Server Error, try again later\n");
-		}
-	}
-})
-
+            await redisClient.set(sessionId, 1);
+            res.status(200)
+                .cookie("id", `${sessionId}`, {
+                    httpOnly: true,
+                    secure: true,
+                    expires: sessionDates(),
+                })
+                .cookie("username", `${userName}`, {
+                    secure: true,
+                    expires: sessionDates(),
+                })
+                .redirect("/");
+        } catch (error) {
+            if (error instanceof Error) {
+                if (error.message === "CREDENTIAL_CONFLICT")
+                    res.status(409).send(
+                        "Sorry, the username you introduced is already in use\n",
+                    );
+                else res.status(505).send("Server Error, try again later\n");
+            }
+        }
+    });
 
 app.use(isValidUrl);
 
-const httpsOptions: https.ServerOptions = {
-	key: readFileSync(BUILD_DIR + "server.key"),
-	cert: readFileSync(BUILD_DIR + "server.cert"),
-}
+const httpsOptions = {
+    key: readFileSync(BUILD_DIR + "server.key"),
+    cert: readFileSync(BUILD_DIR + "server.cert"),
+};
 
-const server = https.createServer(httpsOptions, app);
-server.on("upgrade", async function(req, socket, head) {
-	const unauthorizedResponse = 
-	`HTTP/1.1 401 Unauthorized\r
+const server = createServer(httpsOptions, app);
+server.on("upgrade", async function (req, socket, head) {
+    const unauthorizedResponse = `HTTP/1.1 401 Unauthorized\r
 	WWW-Authenticate: Basic realm="Access to the site"\r
 	Content-Type: text/plain\r
 	Content-Length: 23\r
 	\r
 	Unauthorized access denied`;
 
-	let clientsCookie = req.headers.cookie;
-	if (!clientsCookie) {
-		socket.write(unauthorizedResponse);
-		socket.destroy();
-		return;
-	}
+    let clientsCookie = req.headers.cookie;
+    if (!clientsCookie) {
+        socket.write(unauthorizedResponse);
+        socket.destroy();
+        return;
+    }
 
-	let sessionID = clientsCookie.match(/(?<=id=)\w+/);
-	console.log(clientsCookie, sessionID);
+    let sessionID = clientsCookie.match(/(?<=id=)\w+/);
 
-	if (!sessionID || !sessionID[0]) {
-		socket.write(unauthorizedResponse);
-		socket.destroy();
-		return;
-	} else {
-		let sessionExists = await redisClient.get(sessionID[0]);
-		if (!sessionExists) {
-			socket.write(unauthorizedResponse);
-			socket.destroy();
-			return;
-		} else 
-			return;
-	}
+    if (!sessionID || !sessionID[0]) {
+        socket.write(unauthorizedResponse);
+        socket.destroy();
+    } else {
+        let sessionExists = await redisClient.get(sessionID[0]);
+        if (!sessionExists) {
+            socket.write(unauthorizedResponse);
+            socket.destroy();
+        }
+    }
 });
 
-const wss = new WebSocketServer({ server, path: "/ws"});
+const wss = new WebSocketServer({ server, path: "/ws" });
 
-wss.on("connection", function(ws, req) {
-	ws.on("error", () => console.log("There was an error"));
+wss.on("connection", function (ws, _) {
+    ws.on("error", () => console.log("There was an error"));
 
-	ws.on("message", function(msg) {
-		console.log(new String(msg));
-		// I noticed that is not necesary to parse the RawData type when
-		// sending it with a string in the "send" method
-		wss.clients.forEach((client) => client.send(`${msg}`));
-	})
-
+    ws.on("message", function (msg) {
+        console.log(new String(msg));
+        // I noticed that is not necesary to parse the RawData type when
+        // sending it with a string in the "send" method
+        wss.clients.forEach((client) => client.send(`${msg}`));
+    });
 });
-
-
 
 server.listen(8080, () => {
-	console.log("HTTPs server started");
-	initDataBase().then(() => console.log("initDatabase executed"));
-	redisClient.on("error", err => console.log("Redis client error", err))
-	redisClient.connect();
+    console.log("HTTPs server started");
+    initDataBase().then(() => console.log("initDatabase executed"));
+    redisClient.on("error", (err) => {
+        console.log("Redis client error:");
+        console.error(err);
+    });
+    redisClient.connect();
 });
 
 export default app;
