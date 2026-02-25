671df3664c8c8bc683a8a34285148fdeeb11bb98
Matias A. <lex0ss2062@gmail.com>
2026-02-25 18:12:25 -0300

    feat: Adding a test that verifies that a valid cookie will authorize the access to "/" in the app

--- src/tests/endpoints.test.ts
+++ src/tests/endpoints.test.ts
@@ -1,9 +1,10 @@
 import request from "supertest";
 // import { spawn } from "child_process";
 
-import { app } from "../app";
+import { app, redisClient } from "../app";
 import { ChildProcess, spawn } from "child_process";
 import { exit } from "process";
+import { randomBytes } from "crypto";
 
 class RedisProcess {
     private redisProcess: () => ChildProcess;
@@ -13,45 +14,53 @@ class RedisProcess {
         console.log("redis-server process started");
     }
 
-    public startProcess() {
+    public startProcess(): void {
         this.activeProcess = this.redisProcess();
 
-        // this.activeProcess.stdout!.on("data", (data) => {
-        //     console.log(new String(data));
-        // });
-        //
-        this.activeProcess.stderr!.on("data", (data) => {
-            console.log("An error has ocurred");
-            console.log(new String(data));
-            exit(1);
+        this.activeProcess.once("exit", () => {
+            console.log("Process stopped (alegedlly)");
         });
     }
 
-    public killProcess(): void  {
+    public killProcess(): void {
         if (process === null) {
             console.log("You haven't started the process, aborting");
             exit(1);
         }
-        this.activeProcess!.kill();
-        console.log("Process stopped (alegedlly)");
+
+        this.activeProcess?.kill;
     }
 }
 
 const redisProcess = new RedisProcess();
 
 beforeAll(() => {
-    return redisProcess.startProcess();
+    redisProcess.startProcess();
+    return redisClient.connect();
 });
 
 afterAll(() => {
-    return redisProcess.killProcess();
-
+    redisProcess.killProcess();
 });
+
 
 describe("Testing the GET endpoints of the application", () => {
     it("Get a 302 redirection for normal requests to '/' (with no authorization cookie)", async() => {
         const response = await request(app).get("/");
         expect(response.status).toBe(302);
         expect(response.header.location).toBe("/login");
+    });
+
+    it("Get a 200 response when requesting '/' if you have an authorization cookie", async() => {
+        const sessionId = randomBytes(32).toString("hex");
+        try {
+            await redisClient.set(sessionId, 1);
+        } catch (error) {
+            console.log(`An error has ocurred: ${error}`);
+            exit(1);
+        } 
+
+        const response = await request(app).get("/").set("Cookie", [`id=${sessionId}`]);
+        expect(response.status).toBe(200);
     });
 });
