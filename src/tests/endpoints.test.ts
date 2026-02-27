4ec31c89c77a97e70a8876c040f3dc4bcfa8f020
Matias A. <lex0ss2062@gmail.com>
2026-02-27 09:55:20 -0300

    feat and bugfix: Adding a test for verifying that a login with a non-registered email cannot be authorize and fixing the unhandled error of the redis client to stay open even though the server has stopped by killing the client connection

--- src/tests/endpoints.test.ts
+++ src/tests/endpoints.test.ts
@@ -5,20 +5,26 @@ import { app, redisClient } from "../app";
 import { ChildProcess, spawn } from "child_process";
 import { exit } from "process";
 import { randomBytes } from "crypto";
+import { response } from "express";
 
 class RedisProcess {
     private redisProcess: () => ChildProcess;
     private processHandler: ChildProcess | null = null;
     constructor() {
         this.redisProcess = () => spawn("redis-server");
-        console.log("redis-server process started");
     }
 
     public startProcess(): void {
         this.processHandler = this.redisProcess();
+        console.log("redis-server process started");
 
-        this.processHandler.once("exit", () => {
-            console.log("Process stopped (alegedlly)");
+        // this.processHandler.once("exit", () => {
+        //     console.log("Process stopped (alegedlly)");
+        // });
+        //
+        this.processHandler.on("error", (data) => {
+            console.log(`An error has occurred: ${data}`);
+            exit(1);
         });
     }
 
@@ -28,7 +34,7 @@ class RedisProcess {
             exit(1);
         }
 
-        this.processHandler?.kill;
+        this.processHandler!.kill();
     }
 }
 
@@ -40,17 +46,12 @@ beforeAll(() => {
 });
 
 afterAll(() => {
+    redisClient.destroy();
     redisProcess.killProcess();
 });
 
 
 describe("Testing the GET endpoints of the application", () => {
-    it("Get a 302 redirection for normal requests to '/' (with no authorization cookie)", async() => {
-        const response = await request(app).get("/");
-        expect(response.status).toBe(302);
-        expect(response.header.location).toBe("/login");
-    });
-
     it("Get a 200 response when requesting '/' if you have an authorization cookie", async() => {
         const sessionId = randomBytes(32).toString("hex");
         try {
@@ -64,10 +65,29 @@ describe("Testing the GET endpoints of the application", () => {
         expect(response.status).toBe(200);
     });
 
+    it("Get a 302 redirection for normal requests to '/' (with no authorization cookie)", async() => {
+        const response = await request(app).get("/");
+        expect(response.status).toBe(302);
+        expect(response.header.location).toBe("/login");
+    });
+
     it("Get a 302 if the cookie is not valid", async() => {
         const sessionId = randomBytes(32).toString("hex");
         await request(app).get("/").set("Cookie", [`id=${sessionId}`])
         .expect(302)
         .expect("Location", "/login");
+    });
+
+    it("Get a 401 for introducing credentials to signin into the application", async() => {
+        const response = await request(app)
+        .post('/login')
+        .type('form')
+        .send({
+            email: 'test@example.com',
+            password: 'secret123'
+        });
+
+        expect(response.status).toBe(401);
+        expect(response.text.includes("There's no account with this email yet")).toBe(true);
     });
 });
